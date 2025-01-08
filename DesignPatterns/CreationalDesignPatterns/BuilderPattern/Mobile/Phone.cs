a140e0439af5b33144d73750504d0d8c824c4262
MDBiradar <biradar.cs07@gmail.com>
2025-01-08 23:07:12 +0530

    CSharp-0003 - add builder pattern phone example

---
+++ DesignPatterns/CreationalDesignPatterns/BuilderPattern/Mobile/Phone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp.DesignPatterns.CreationalDesignPatterns.BuilderPattern.Mobile
+{
+    internal class Phone
+    {
+        private string? _brand;
+        private string? _model;
+        private string? _os;
+        private decimal _price;
+
+        public Phone(string? brand, string? model, string? os, decimal price)
+        {
+            this._brand = brand;
+            this._model = model;
+            this._os = os;
+            this._price = price;
+        }
+        public override string? ToString()
+        {
+            return $"Brand: {this._brand}, Model: {this._model}, OS: {this._os}, Price: {this._price}";
+        }
+    }
+}
---
+++ DesignPatterns/CreationalDesignPatterns/BuilderPattern/Mobile/PhoneBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp.DesignPatterns.CreationalDesignPatterns.BuilderPattern.Mobile
+{
+    internal class PhoneBuilder
+    {
+        private string? _brand;
+        private string? _model;
+        private string? _os;
+        private decimal _price;
+
+        public string Brand
+        {            
+            set { _brand = value; }            
+        }
+        public string Model
+        {
+            set { _model = value; }
+        }
+
+        public string Os
+        {
+            set { _os = value; }
+        }
+        public decimal Price
+        {
+            set { _price = value; }
+        }
+
+        public Phone getPhone()
+        {
+            return new Phone(this._brand , this._model, this._os, this._price);
+        }
+    }
+}
